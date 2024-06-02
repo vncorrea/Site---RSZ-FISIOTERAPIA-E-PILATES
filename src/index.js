313ed774dd6d81940ba2fc4cbef7c698f31ab4be
vncorrea <vinicorreag2001@gmail.com>
2024-06-02 20:35:11 -0300

    feat: ajustes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ const clickHabilidades = function (e) {
     let todasDivs = document.querySelectorAll(".text-habilidades");
 
     todasDivs.forEach(function (div) {
+      let habilidadeSelecionada = div.getAttribute("data-habilidade");
+
+      let infoHabilidade = document.querySelector(
+        ".info_habilidade_" + habilidadeSelecionada
+      );
+
+      infoHabilidade.classList.add("d-none");
+
       div.classList.remove("border-primary");
       div.classList.remove("text-primary");
       div.classList.remove("border-bottom");
@@ -15,5 +23,19 @@ const clickHabilidades = function (e) {
     texto_div.classList.add("text-primary");
     texto_div.classList.remove("text-white");
     texto_div.classList.toggle("border-bottom");
+
+    selecionarHabilidade(texto_div);
   }
+};
+
+const selecionarHabilidade = function (habilidade) {
+  let habilidadeSelecionada = habilidade.getAttribute("data-habilidade");
+
+  let infoHabilidade = document.querySelector(
+    ".info_habilidade_" + habilidadeSelecionada
+  );
+
+  console.log(infoHabilidade);
+
+  infoHabilidade.classList.remove("d-none");
 };
